f4b2cf5a5ad772c6ef93bf2d203b678f4a8367f7
goodie77codes <shikamaru1082@gmail.com>
2026-01-27 08:58:18 -0800

    cors allowed in contact post endpoint

--- routes/contact.js
+++ routes/contact.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const cors = require("cors");
 const auth = require("../middlewares/auth.js");
 const {
   createContact,
@@ -9,7 +10,7 @@ const {
   updateContact,
 } = require("../controllers/contact.js");
 
-router.post("/:contactListId", createContact);
+router.post("/:contactListId", cors(), createContact);
 router.get("/", auth, getAllContact);
 router.delete("/:contactId", auth, deleteContact);
 router.get("/:contactId", auth, getContactById);
